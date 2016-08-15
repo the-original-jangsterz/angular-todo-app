9e08e4d19e8a48e9b7282f1e8a91416680f5c564
Peter Jang <peter.x.jang@gmail.com>
2016-08-15 18:13:47 -0500

    Add input to add new todo

--- app/assets/javascripts/todos_ctrl.js
+++ app/assets/javascripts/todos_ctrl.js
@@ -1,3 +1,5 @@
+/* global angular */
+
 (function() {
   angular.module('app').controller('todosCtrl', function($scope) {
     $scope.todos = [
@@ -5,5 +7,12 @@
       'Mow the lawn',
       'Be sure to drink your ovaltine'
     ];
+
+    $scope.addTodo = function(inputTodo) {
+      if (inputTodo !== '') {
+        $scope.todos.push(inputTodo);
+        $scope.newTodo = '';
+      }
+    };
   });
 })();
